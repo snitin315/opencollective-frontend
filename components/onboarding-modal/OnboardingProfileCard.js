800d6a9cb6f7a0f241dd121ec533c181b335cca4
Kate Beard <asouthernbellein@gmail.com>
2020-03-18 22:57:51 +0000

    clean up

--- components/onboarding-modal/OnboardingProfileCard.js
+++ components/onboarding-modal/OnboardingProfileCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Flex, Box } from '@rebass/grid';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { FormattedMessage } from 'react-intl';
 
 import Avatar from '../../components/Avatar';
